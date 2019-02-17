712d4e9434408bf10be305bf978be30afd04d616
GooRiOn <foreverframenet@gmail.com>
2019-02-17 13:58:30 +0100

    Fixed build script

--- scripts/dotnet-build.sh
+++ scripts/dotnet-build.sh
@@ -6,4 +6,4 @@ case "$TRAVIS_BRANCH" in
     ;;
 esac
 
-dotnet build -c Release --no-cache --source https://www.myget.org/F/dnc-dshop/api/v3/index.json --source https://api.nuget.org/v3/index.json
+dotnet build -c Release --no-cache --source https://www.myget.org/F/dnc-dshop$MYGET_ENV/api/v3/index.json --source https://api.nuget.org/v3/index.json
