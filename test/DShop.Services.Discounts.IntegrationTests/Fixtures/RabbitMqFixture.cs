52168ee665df33364b966d7d7c46364bd6ed9ac5
GooRiOn <foreverframenet@gmail.com>
2019-03-03 23:41:13 +0100

    Added missing publish config

--- test/DShop.Services.Discounts.IntegrationTests/Fixtures/RabbitMqFixture.cs
+++ test/DShop.Services.Discounts.IntegrationTests/Fixtures/RabbitMqFixture.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
+using DShop.Common;
 using DShop.Common.Messages;
 using DShop.Common.RabbitMq;
 using DShop.Services.Discounts.Domain;
@@ -38,7 +40,8 @@ namespace DShop.Services.Discounts.IntegrationTests.Fixtures
         }
 
         public Task PublishAsync<TMessage>(TMessage message) where TMessage : class
-            => _client.PublishAsync(message);
+            => _client.PublishAsync(message, ctx => 
+                ctx.UseMessageContext(CorrelationContext.Empty).UsePublishConfiguration(p => p.WithRoutingKey(GetRoutingKey(@message))));
         
         public async Task<TaskCompletionSource<Discount>> SubscribeAndGetAsync<TEvent>(
             Func<Guid, TaskCompletionSource<Discount>, Task> onMessageReceived, Guid id) where TEvent : IEvent
@@ -55,6 +58,14 @@ namespace DShop.Services.Discounts.IntegrationTests.Fixtures
                                 .WithDurability(false)
                                 .WithName(guid))));
             return taskCompletionSource;
+        }
+        
+        private string GetRoutingKey<T>(T message)
+        {
+            var @namespace = "discounts";
+            @namespace = string.IsNullOrWhiteSpace(@namespace) ? string.Empty : $"{@namespace}.";
+
+            return $"{@namespace}{typeof(T).Name.Underscore()}".ToLowerInvariant();
         }
 
         protected virtual void Dispose(bool disposing)
